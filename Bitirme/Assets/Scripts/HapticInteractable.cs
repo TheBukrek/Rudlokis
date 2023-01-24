3e2e3dde2e6f0bc298845c2655a5823d094d6f12
EmirFirat <emrfrt09@gmail.com>
2023-01-24 17:41:22 +0300

    Haptic with gun shooting added
    Co-Authored-By: Utku <71753904+welsdarte@users.noreply.github.com>

---
+++ Bitirme/Assets/Scripts/HapticInteractable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[System.Serializable]
+
+public class Haptic
+{
+    [Range(0, 1)]
+    public float intensity;
+    public float duration;
+
+
+    public void TriggerHaptic(BaseInteractionEventArgs eventArgs)
+    {
+
+        if (eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
+        {
+            Debug.Log("haptic");
+            TriggerHaptic(controllerInteractor.xrController);
+        }
+    }
+    public void TriggerHaptic(XRBaseController controller)
+    {
+        if (intensity > 0)
+        {
+
+            controller.SendHapticImpulse(intensity, duration);
+        }
+    }
+
+}
+public class HapticInteractable : MonoBehaviour
+{
+    public Haptic hapticOnActive;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.activated.AddListener(hapticOnActive.TriggerHaptic);
+    }
+
+    
+}
