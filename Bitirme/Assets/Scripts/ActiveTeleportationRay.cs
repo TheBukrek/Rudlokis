498fdb036b0182e32a0a17ea89fd281ee247f8d5
Onur Adıgüzel <onur.adiguzel@hotmail.com>
2023-01-24 16:09:27 +0300

    Force Pull

--- Bitirme/Assets/Scripts/ActiveTeleportationRay.cs
+++ Bitirme/Assets/Scripts/ActiveTeleportationRay.cs
@@ -6,18 +6,18 @@ using UnityEngine.InputSystem;
 
 public class ActiveTeleportationRay : MonoBehaviour
 {
-    public GameObject lefttp;
-    public GameObject righttp;
+    public GameObject leftTeleportationRay;
+    public GameObject rightTeleportationRay;
         
-    public InputActionProperty leftactive;
-    public InputActionProperty rightactive;
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
 
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
-
-
+    
     public XRRayInteractor leftRay;
     public XRRayInteractor rightRay;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -27,13 +27,11 @@ public class ActiveTeleportationRay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumer, out bool leftValid);
-        // lefttp.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftactive.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        leftTeleportationRay.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
         
-        // bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumer, out bool rightValid);
-        // righttp.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightactive.action.ReadValue<float>() > 0.1f);
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+        rightTeleportationRay.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
 
-        lefttp.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftactive.action.ReadValue<float>() > 0.1f);
-        righttp.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightactive.action.ReadValue<float>() > 0.1f);
     }
 }
