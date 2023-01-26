807ebd71dc57de322d3a708080777ede1efd9e3b
Onur Adıgüzel <onur.adiguzel@hotmail.com>
2023-01-26 18:14:25 +0300

    Simple Ammo Counter On Pistol
    No reload yet, and haptic doesn't turn off when at 0 rounds in the magazine.

--- Bitirme/Assets/Scripts/FireBulletOnActivate.cs
+++ Bitirme/Assets/Scripts/FireBulletOnActivate.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -8,12 +9,19 @@ public class FireBulletOnActivate : MonoBehaviour
     public GameObject bullet;
     public Transform spawnPoint;
     public float fireSpeed;
-    
+    public int ammoCount;
+    public TextMeshProUGUI ammoCountText;
+    private HapticInteractable hapticInteractable;
+
+
     // Start is called before the first frame update
     void Start()
     {
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(FireBullet);
+        
+        ammoCountText.text = ammoCount.ToString();
+        hapticInteractable = GetComponent<HapticInteractable>();
     }
 
     // Update is called once per frame
@@ -24,9 +32,19 @@ public class FireBulletOnActivate : MonoBehaviour
 
     public void FireBullet(ActivateEventArgs args)
     {
-        GameObject spawnedBullet = Instantiate(bullet);
-        spawnedBullet.transform.position = spawnPoint.position;
-        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
-        Destroy(spawnedBullet, 5f);
+        if (ammoCount > 0)
+        {
+            GameObject spawnedBullet = Instantiate(bullet);
+            spawnedBullet.transform.position = spawnPoint.position;
+            spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+            ammoCount--;
+            ammoCountText.text = ammoCount.ToString();
+            Destroy(spawnedBullet, 5f);
+        }
+        else
+        {
+            if(hapticInteractable.enabled)
+                GetComponent<HapticInteractable>().enabled = false;
+        }
     }
 }
