c3b559c0aeeb5aa93ca6506301f64174ab9bb3da
Arda Cakiroglu <ardayigitcakiroglu@gmail.com>
2023-02-14 19:40:47 +0300

    Sound Settings Added

---
+++ Bitirme/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("volume"))
+        {
+            PlayerPrefs.SetFloat("volume", 1f);
+            LoadVolume();
+        }
+        else
+        {
+            LoadVolume();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        SaveVolume();
+    }
+
+    private void LoadVolume()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume", 1f);
+    }
+
+    private void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+    }
+}
