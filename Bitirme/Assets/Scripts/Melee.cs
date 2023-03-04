ae64313868871443da78cb1777e803913cd91fa9
EmirFirat <emrfrt09@gmail.com>
2023-03-04 23:33:32 +0300

    Two Hand Weapon
    !!buggy
    
    Co-Authored-By: Utku <71753904+welsdarte@users.noreply.github.com>

--- Bitirme/Assets/Scripts/Melee.cs
+++ Bitirme/Assets/Scripts/Melee.cs
@@ -13,19 +13,21 @@ public class Melee : MonoBehaviour
     public float swingAmountCurrent  = 0;
     public float swingAmountMinimumVelocity = 1;
 
-    private GameObject[] multipleEnemy;
 
     public Transform spawnPoint;
-    public Vector3 midVector;
-    public Vector3 midPoint;
-    public Vector3 startPoint;
-    public Vector3 swingStartVector;
 
-    public Vector3 startRotation;
-    public Vector3 endRotation;
+    private Vector3 midPoint;
+    private Vector3 startPoint;
+    private Vector3 swingStartVector;
+
+    private Vector3 startRotation;
+    
+    private Vector3 endRotation;
+
+    private Quaternion midRotation;
+    private bool midRotationAssigned;
 
 
-    public Vector3 closeEnemy;
 
     public GameObject projectilePrefab;
 
@@ -63,32 +65,26 @@ public class Melee : MonoBehaviour
 
             }
 
-            swingAmountCurrent += Time.deltaTime * velocity;
+            swingAmountCurrent += Time.deltaTime;
+
+            if( swingAmountCurrent >= swingAmountRequired / 2 ){
+                if( midRotationAssigned == false){
+                    midRotation = spawnPoint.rotation;
+                    midPoint = spawnPoint.position;
+                    midRotationAssigned = true;
+                }
+               
+                
+            }else{
+                midRotationAssigned = false;
+            }
+
 
             if (swingAmountCurrent >= swingAmountRequired) {
 
-                midVector = (spawnPoint.forward.normalized + swingStartVector.normalized).normalized;
-                endRotation = spawnPoint.eulerAngles;
-                float angle = Vector3.Angle(swingStartVector, spawnPoint.forward);
-                //endRotation = new Vector3(endRotation.x - angle/2, endRotation.y, endRotation.z);
-                midPoint = (spawnPoint.position + startPoint)/2;
                 swingAmountCurrent = 0f;
-
-                
-                //Vector3 midRotation = new Vector3( (startRotation.x + endRotation.x)/2f, (startRotation.y + endRotation.y) / 2f, (startRotation.z + endRotation.z) /2f    );
-                Quaternion midRotation = calcAvg(quaternionList);
-
-                Vector3 proj = Vector3.Project(spawnPoint.forward, swingStartVector);
-                
-                
-                
-
-
-
-                GameObject newProjectile = Instantiate<GameObject>(projectilePrefab, midPoint, Quaternion.Euler(endRotation)) ;
+                GameObject newProjectile = Instantiate<GameObject>(projectilePrefab, midPoint, midRotation) ;
                 newProjectile.GetComponent<SwordProjectile>().speed = Math.Min(20, velocity * 20);
-
-
 
             }
 
@@ -100,56 +96,11 @@ public class Melee : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.CompareTag("Enemy"))
-        {
-            if (velocity >= 2f)
-            {
-                //Debug.Log("velocity:   " + map(velocity, 2, 10, 1, 2));
-                EnemyScript enemy = collision.transform.GetComponent<EnemyScript>();
-
-                enemy.Damage(map(velocity,2,10,1,2) * 30);
-            }
-        }
-    }
 
     float map(float s, float a1, float a2, float b1, float b2)
     {
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
-    public Transform getClosestEnemy()
-    {
-        multipleEnemy = GameObject.FindGameObjectsWithTag("Enemy");
-        float closestdis = Mathf.Infinity;
-        Transform trans = null;
-
-        foreach (GameObject go in multipleEnemy)
-        {
-            float currentDis;
-            currentDis = Vector3.Distance(transform.position, go.transform.position);
-            if (currentDis < closestdis)
-            {
-                closestdis = currentDis;
-                trans = go.transform;
-            }
-        }
-        return trans;
-
-    }
-
-    private Quaternion calcAvg(List<Quaternion> rotationlist)
-    {
-        if (rotationlist.Count == 0)
-            throw new ArgumentException();
-
-        float x = 0, y = 0, z = 0, w = 0;
-        foreach (Quaternion q in rotationlist)
-        {
-            x += q.x; y += q.y; z += q.z; w += q.w;
-        }
-        float k = 1.0f / Mathf.Sqrt(x * x + y * y + z * z + w * w);
-        return new Quaternion(x * k, y * k, z * k, w * k);
-    }
+    
 
 }
--- Bitirme/Assets/Scripts/XRGrabInteractableTwoAttach.cs
+++ Bitirme/Assets/Scripts/XRGrabInteractableTwoAttach.cs
@@ -6,22 +6,76 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRGrabInteractableTwoAttach : XRGrabInteractable
 {
+    public List<XRSimpleInteractable> secondInt = new List<XRSimpleInteractable>();
 
+    private Quaternion attachinitial;
+    private XRBaseInteractor secondInteractor;
 
     public Transform leftAttachTransform;
     public Transform rightAttachTransform;
 
+
+    void Start()
+    {
+        foreach(var item in secondInt)
+        {
+            item.onSelectEntered.AddListener(OnSecondHandGrab);
+            item.onSelectExited.AddListener(OnSecondHandRelease);
+        }
+    }
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {   
+        if(secondInteractor && selectingInteractor)
+        {
+            selectingInteractor.attachTransform.rotation = Quaternion.LookRotation(secondInteractor.attachTransform.position - selectingInteractor.attachTransform.position);
+
+        }
+        base.ProcessInteractable(updatePhase);
+    }
+    public void OnSecondHandGrab(XRBaseInteractor interactor)
+    {
+        Debug.Log("second grab");
+        secondInteractor = interactor;
+    }
+
+    public void OnSecondHandRelease(XRBaseInteractor interactor)
+    {
+        Debug.Log("second rel");
+        secondInteractor = null;
+    }
+
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        Debug.Log("first grab");
+        
+        base.OnSelectEntered(interactor);
+        attachinitial = interactor.attachTransform.localRotation;
+
+    }
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
 
-        if(args.interactorObject.transform.CompareTag("Left Hand"))
+        if (args.interactorObject.transform.CompareTag("Left Hand"))
         {
             attachTransform = leftAttachTransform;
         }
-        else if(args.interactorObject.transform.CompareTag("Right Hand"))
+        else if (args.interactorObject.transform.CompareTag("Right Hand"))
         {
             attachTransform = rightAttachTransform;
         }
         base.OnSelectEntered(args);
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        Debug.Log("firts rel");
+        base.OnSelectExited(interactor);
+        secondInteractor = null;
+        interactor.attachTransform.localRotation = attachinitial;
+    }
+    public override bool IsSelectableBy(XRBaseInteractor interactor)
+    {
+        bool isalreadygrabed = selectingInteractor && !interactor.Equals(selectingInteractor);
+        return base.IsSelectableBy(interactor) && !isalreadygrabed;
     }
 }
