95789050d4924b9c0b51429533acb467524a95b6
Arda Cakiroglu <ardayigitcakiroglu@gmail.com>
2023-03-11 18:56:47 +0300

    GameManager singleton and level maths
    Co-Authored-By: Utku <71753904+welsdarte@users.noreply.github.com>

---
+++ Bitirme/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _instance;
+    public static GameManager Instance{
+
+        get{
+            if (_instance == null) {
+                Debug.LogError("Game Manager is null");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
+    public int level;
+    public float xp;
+
+    public float requiredXpMultiplier;
+    public float requiredXp;
+
+
+    public float enemyXpReward1 = 180;
+
+    public void AddXp(float xpAmount) {
+        float newXp = xpAmount + xp;
+
+        level = Mathf.FloorToInt (Mathf.Sqrt(newXp) * requiredXpMultiplier);
+        xp = newXp;
+    }
+
+    public float CalculateRequiredXp()
+    {
+        float result = Mathf.Pow((level + 1) / requiredXpMultiplier, 2);
+        requiredXp = result;
+        return requiredXp;
+        
+    }
+
+
+
+    private void Update()
+    {
+        Debug.Log("level: "+level);
+        Debug.Log("xp: " + xp);
+        Debug.Log("requiredXp: " + requiredXp);
+        CalculateRequiredXp();
+        AddXp(Time.deltaTime * 100);
+    }
+}
