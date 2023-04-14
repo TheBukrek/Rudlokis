0672a78d42972a8a33787e4f41e4da4e38bbd1b5
Arda Cakiroglu <ardayigitcakiroglu@gmail.com>
2023-04-14 20:42:20 +0300

    online movement

---
+++ Bitirme/Assets/PlayerMovementController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+public class PlayerMovementController : NetworkBehaviour
+{
+    public float Speed = 0.1f;
+    public GameObject PlayerModel;
+    // Start is called before the first frame update
+    public void Movement() {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * Speed;
+    }
+
+    public void Start()
+    {
+        PlayerModel.SetActive(false);
+
+    }
+    public void SetPosition()
+    {
+        transform.position = new Vector3(Random.Range(-5, 5), 0.8f, Random.Range(-15, 7));
+    }
+    private void Update()
+    {
+        if (PlayerModel.activeSelf == false && SceneManager.GetActiveScene().name == "OnlineDeneme")
+        {
+            SetPosition();
+             PlayerModel.SetActive(true);
+        }
+        if (isOwned)
+        {
+            Movement();
+        }
+    }
+}
